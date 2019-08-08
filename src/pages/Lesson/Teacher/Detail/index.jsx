68b8db744a0021c288c9ea00df897ac734460349
BetterGG <1285462534@qq.com>
2019-08-08 23:47:43 +0800

    feat -m 修改了teacher版块

---
+++ src/pages/Lesson/Teacher/Detail/index.jsx
@@ -0,0 +1,40 @@
+import React, { PureComponent } from 'react';
+import { PageHeaderWrapper } from '@ant-design/pro-layout';
+import { Pagination } from 'antd';
+import ListShow from './Components/ListShow';
+import QuestionAnswer from './Components/QuestionAnswer';
+import styles from './index.less';
+
+class Index extends PureComponent {
+  onShowSizeChange = () => {};
+
+  render() {
+    return (
+      <div>
+        <PageHeaderWrapper />
+        <div>
+          <ListShow />
+          <div className={styles.playBox}>
+            {/* eslint-disable-next-line jsx-a11y/media-has-caption */}
+            <video
+              className={styles.play}
+              controls="controls"
+              src="http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400"
+            />
+          </div>
+          <QuestionAnswer />
+          <Pagination
+            showSizeChanger
+            pageSizeOptions={['10', '20', '30', '40', '50']}
+            onShowSizeChange={this.onShowSizeChange}
+            defaultCurrent={3}
+            total={500}
+            style={{ marginTop: '20px' }}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Index;
---
+++ src/pages/Lesson/Teacher/List/Components/ListSearch/index.jsx
@@ -0,0 +1,121 @@
+import React, { PureComponent } from 'react';
+import { Row, Col, Input, Button, Select, DatePicker } from 'antd';
+import moment from 'moment';
+import styles from './index.less';
+import { standardToNum } from '@/utils/format';
+
+const { Option } = Select;
+const { RangePicker } = DatePicker;
+const dateFormat = 'YYYY/MM/DD';
+
+class ListSearch extends PureComponent {
+  state = {
+    title: '关于四险一金',
+    author: '王一',
+    type: '民事',
+    start: '2019/01/01',
+    end: '2019/01/01',
+  };
+
+  onChange = (value, type) => {
+    this.setState({
+      [type]: value,
+    });
+  };
+
+  onChangePicker = e => {
+    let start = standardToNum(e[0]._d);
+    let end = standardToNum(e[1]._d);
+    this.setState({
+      start,
+      end,
+    });
+  };
+
+  onSearch = () => {
+    this.props.onSearchParam(this.state);
+  };
+
+  onReset = () => {
+    this.setState({
+      title: '',
+      author: '',
+      type: '',
+      start: '2018/01/01',
+      end: '2018/01/01',
+    });
+  };
+
+  render() {
+    let { title, author, type, start, end } = this.state;
+    return (
+      <div className={styles.selectBox}>
+        <Row gutter={32}>
+          <Col span={6} className={styles.item}>
+            <div className={styles.rowBox}>
+              <div className={styles.conL}>标题</div>
+              <div className={styles.rowCon}>
+                <Input placeholder="请输入" value={title} onChange={e => this.onChange(e.target.value, 'title')} />
+              </div>
+            </div>
+          </Col>
+          <Col span={6} className={styles.item}>
+            <div className={styles.rowBox}>
+              <div className={styles.conL}>作者</div>
+              <div className={styles.rowCon}>
+                <Input placeholder="请输入" value={author} onChange={e => this.onChange(e.target.value, 'author')} />
+              </div>
+            </div>
+          </Col>
+          <Col span={6} className={styles.item}>
+            <div className={styles.rowBox}>
+              <div className={styles.conL}>视频类型</div>
+              <div className={styles.rowCon}>
+                <Select
+                  showSearch
+                  style={{ width: '100%' }}
+                  placeholder="Select a person"
+                  optionFilterProp="children"
+                  filterOption={(input, option) =>
+                    option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+                  }
+                  value={type}
+                  onChange={value => this.onChange(value, 'type')}
+                >
+                  <Option value="婚姻">婚姻</Option>
+                  <Option value="刑事">刑事</Option>
+                  <Option value="民事">民事</Option>
+                </Select>
+              </div>
+            </div>
+          </Col>
+          <Col span={6} className={styles.item}>
+            <div className={styles.rowBox}>
+              <div className={styles.conL}>发布时间</div>
+              <div className={styles.rowCon}>
+                <RangePicker
+                  size="default"
+                  format="YYYY-MM-DD"
+                  onChange={this.onChangePicker}
+                  value={[moment(start, dateFormat), moment(end, dateFormat)]}
+                />
+              </div>
+            </div>
+          </Col>
+          <Col span={6} className={styles.item}>
+            <div>
+              <Button type="primary" icon="search" className={styles.btnMr} onClick={this.onSearch}>
+                搜索
+              </Button>
+              <Button type="primary" onClick={this.onReset}>
+                重置
+              </Button>
+            </div>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+}
+
+export default ListSearch;
