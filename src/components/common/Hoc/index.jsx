75f067634fe79edc0e61969035824fb47171cdad
zhuwenhan <zhuwenhan@dxy.cn>
2019-08-13 16:21:16 +0800

    feat -l 调整新闻列表页;优化部分代码

--- src/components/common/Hoc/index.jsx
+++ src/components/common/Hoc/index.jsx
@@ -1,11 +1,11 @@
 import React, { PureComponent } from 'react';
 
-export default function HocComponent(WrappedComponent, WrappedProps, functional = true) {
-  const FnComponent = props => <WrappedComponent {...WrappedProps} {...props} />;
-  class ClassComponent extends PureComponent {
-    render() {
-      return <WrappedComponent {...WrappedProps} {...this.props} />;
-    }
-  }
-  return functional ? FnComponent : ClassComponent;
+export default function Hoc(WrappedComponent, WrappedProps, functional = true) {
+  return functional
+    ? props => <WrappedComponent {...WrappedProps} {...props} />
+    : class ClassComponent extends PureComponent {
+        render() {
+          return <WrappedComponent {...WrappedProps} {...this.props} />;
+        }
+      };
 }
