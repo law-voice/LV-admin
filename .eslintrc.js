aaf42ac7551241f8d58fb8967b7904de93f70512
BetterGG <1285462534@qq.com>
2020-05-22 01:32:03 +0800

    fix -g 解决lintStage问题和三元运算符不可使用的问题

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,27 +6,33 @@ module.exports = {
   rules: {
     ...strictEslint.rules,
     'max-len': 0,
-    //函数参数不允许二次赋值
+    // 函数参数不允许二次赋值
     'no-param-reassign': 0,
-    //parseInt 必须主动传入第二个参数表示进制
+    // parseInt 必须主动传入第二个参数表示进制
     radix: 0,
-    //禁止使用位操作符
+    // 禁止使用位操作符
     'no-bitwise': 0,
     // 禁用 js 特性语法
     'no-restricted-syntax': 0,
     // 禁止以_开头的属性名
     'no-underscore-dangle': 0,
-    //禁止匿名函数
+    // 禁止匿名函数
     'func-names': 0,
-    //禁止自增运算符
+    // 禁止自增运算符
     'no-plusplus': 0,
-    //禁止使用对象某些属性
+    // 禁止使用对象某些属性
     'no-restricted-properties': 0,
     // 未重新赋值的变量用 const 声明
     'prefer-const': 0,
     'import/no-extraneous-dependencies': 0,
     // async 需要返回值
     'consistent-return': 0,
+    // 取消禁用三元运算符
+    'no-nested-ternary': 0,
+    // 取消let后只能声明一个的设置
+    'one-var': 0,
+    // 取消禁用console
+    'no-console': 0,
   },
   globals: {
     ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION: true,
